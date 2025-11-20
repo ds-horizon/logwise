b1bd5de38828890907b0d24eb8473d7ed2b45b54
Onkar Sarvade <onkar.sarvade@dream11.com>
2025-11-20 12:05:34 +0530

    FIxed spark issue

--- spark/src/main/java/com/logwise/spark/schema/Schema.java
+++ spark/src/main/java/com/logwise/spark/schema/Schema.java
@@ -11,14 +11,9 @@ public class Schema {
   public StructType getVectorApplicationLogsSchema() {
     return new StructType()
         .add(Constants.APPLICATION_LOG_COLUMN_MESSAGE, DataTypes.StringType)
-        .add(Constants.APPLICATION_LOG_COLUMN_DDTAGS, DataTypes.StringType)
         .add(Constants.APPLICATION_LOG_COLUMN_TIMESTAMP, DataTypes.StringType)
         .add(Constants.APPLICATION_LOG_COLUMN_ENVIRONMENT_NAME, DataTypes.StringType)
         .add(Constants.APPLICATION_LOG_COLUMN_SERVICE_NAME, DataTypes.StringType)
-        .add(Constants.APPLICATION_LOG_COLUMN_COMPONENT_TYPE, DataTypes.StringType)
-        .add(Constants.APPLICATION_LOG_COLUMN_HOSTNAME, DataTypes.StringType)
-        .add(Constants.APPLICATION_LOG_COLUMN_DDSOURCE, DataTypes.StringType)
-        .add(Constants.APPLICATION_LOG_COLUMN_SOURCE_TYPE, DataTypes.StringType)
-        .add(Constants.APPLICATION_LOG_COLUMN_STATUS, DataTypes.StringType);
+        .add(Constants.APPLICATION_LOG_COLUMN_COMPONENT_TYPE, DataTypes.StringType);
   }
 }
